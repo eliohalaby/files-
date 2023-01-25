a1d6650e41c2189cbeccf7996a237902cc134b57
Elio Halaby <elio.halaby@lau.edu>
2023-01-25 07:14:27 -0800

    My first commit

---
+++ NKF.java
@@ -0,0 +1,12 @@
+public class NKF{
+	public static void main ( String [] args){
+System.out.println("Hello NFK nice to meet you");
+System.out.print("My name is Mr. Halaby");
+System.out.println(" how can i help you");
+System.out.print("Do you want to play hide and seek, or you want to go fishing???");
+
+}
+
+}
+
+
